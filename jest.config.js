9f3f86fca12401e4f613193745ad25dd31a93da8
George Pricop <96602369+Gzeu@users.noreply.github.com>
2025-09-22 14:16:27 +0300

    Add Jest configuration for proper testing setup

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,29 @@
 module.exports = {
-    testEnvironment: 'node',
-    coverageDirectory: 'coverage',
-    collectCoverageFrom: [
-        'server.js',
-        '!node_modules/**',
-        '!coverage/**',
-        '!logs/**'
-    ],
-    testMatch: [
-        '**/__tests__/**/*.js',
-        '**/?(*.)+(spec|test).js'
-    ],
-    verbose: true,
-    setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
-    testTimeout: 30000
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  testMatch: [
+    '**/__tests__/**/*.js',
+    '**/?(*.)+(spec|test).js'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
+  testTimeout: 30000,
+  verbose: true,
+  collectCoverageFrom: [
+    'server.js',
+    'enhanced-features.js',
+    'api-improvements.js',
+    '!node_modules/**',
+    '!coverage/**',
+    '!.github/**'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50
+    }
+  }
 };
